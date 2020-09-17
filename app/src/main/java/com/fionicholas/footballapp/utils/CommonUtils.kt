fa23ad1d840cb390d5105b7f98f07e0b1bfd6693
fionicholas <fiorentcheaters@gmail.com>
2020-09-18 00:37:42 +0700

    add ext function format date

--- app/src/main/java/com/fionicholas/footballapp/utils/CommonUtils.kt
+++ app/src/main/java/com/fionicholas/footballapp/utils/CommonUtils.kt
@@ -2,6 +2,8 @@ package com.fionicholas.footballapp.utils
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun AppCompatActivity.replaceFragment(viewRes: Int, fragment: Fragment, addToBackStack: Boolean) {
     val transaction = this.supportFragmentManager.beginTransaction()
@@ -16,4 +18,18 @@ fun AppCompatActivity.replaceFragment(viewRes: Int, fragment: Fragment, addToBac
 
     // Commit the transaction
     transaction.commit()
+}
+
+fun String.toBaseDateFormat() : String {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+    var myDate: Date? = null
+    try {
+        myDate = dateFormat.parse(this)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    val newFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+    return newFormat.format(myDate)
 }
