6afe8360211e43b0edbb4a90215c1fae3ca9872c
fionicholas <fiorentcheaters@gmail.com>
2020-09-20 14:37:01 +0700

    add UI team favorite with data dummy

--- app/src/main/java/com/fionicholas/footballapp/ui/team/DetailTeamActivity.kt
+++ app/src/main/java/com/fionicholas/footballapp/ui/team/DetailTeamActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
@@ -27,6 +29,10 @@ class DetailTeamActivity : BaseActivity() {
     }
 
     private val viewModel: TeamViewModel by inject()
+
+    private var menuItem: Menu? = null
+
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,5 +87,35 @@ class DetailTeamActivity : BaseActivity() {
 
     private val onMessageErrorObserver = Observer<String> {
         Log.e("TAG", "onMessageError $it")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_favorite, menu)
+        menuItem = menu
+        setFavorite()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            R.id.menuFavorite -> {
+                isFavorite = !isFavorite
+                setFavorite()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setFavorite() {
+        if (isFavorite)
+            menuItem?.getItem(0)?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_favorite_white)
+        else
+            menuItem?.getItem(0)?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_favorite_border_white)
     }
 }
