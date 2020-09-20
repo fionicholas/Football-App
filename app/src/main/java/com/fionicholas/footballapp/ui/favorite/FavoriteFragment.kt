c271c750e87f8612cd20f61b5522f8cea6c67a51
fionicholas <fiorentcheaters@gmail.com>
2020-09-20 14:38:21 +0700

    adjust UI FavoriteFragment.kt

--- app/src/main/java/com/fionicholas/footballapp/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/fionicholas/footballapp/ui/favorite/FavoriteFragment.kt
@@ -1,13 +1,16 @@
 package com.fionicholas.footballapp.ui.favorite
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.fionicholas.footballapp.R
+import com.fionicholas.footballapp.base.BaseFragment
+import com.fionicholas.footballapp.ui.general.adapter.GeneralPagerAdapter
+import kotlinx.android.synthetic.main.fragment_favorite.*
+import kotlinx.android.synthetic.main.layout_toolbar_main.*
 
-class FavoriteFragment : Fragment() {
+class FavoriteFragment : BaseFragment() {
 
     companion object {
         @JvmStatic
@@ -24,6 +27,36 @@ class FavoriteFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupToolbar(toolbar, getString(R.string.label_favorite), false)
+        tvToolbarTitle.text = getString(R.string.label_favorite)
+        setupViewPagerFavorite()
+    }
+
+    private fun setupViewPagerFavorite() {
+        val pages = listOf(
+            MatchFavoriteFragment.newInstance(),
+            TeamFavoriteFragment.newInstance()
+        )
+
+        val pageTitles = listOf(
+            getString(R.string.title_tab_match),
+            getString(R.string.title_tab_team)
+        )
+
+        vpFavorite.apply {
+            adapter = GeneralPagerAdapter(
+                childFragmentManager,
+                pages = pages,
+                title = pageTitles
+            )
+            offscreenPageLimit = 2
+        }
+
+        tabFavorite.setupWithViewPager(vpFavorite)
     }
 
 }
