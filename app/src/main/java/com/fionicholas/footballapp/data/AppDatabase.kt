15c58d031299b8d109d2b395a481df592760d2da
fionicholas <fiorentcheaters@gmail.com>
2020-09-20 14:39:19 +0700

    create data flow match favorite

--- app/src/main/java/com/fionicholas/footballapp/data/AppDatabase.kt
+++ app/src/main/java/com/fionicholas/footballapp/data/AppDatabase.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.fionicholas.footballapp.data.movie.local.MovieDao
-import com.fionicholas.footballapp.data.movie.local.response.MovieFavorite
+import com.fionicholas.footballapp.data.matchfavorite.local.MatchFavoriteDao
+import com.fionicholas.footballapp.data.matchfavorite.local.response.MatchFavorite
 
-@Database(entities = [MovieFavorite::class], version = 1)
+@Database(entities = [MatchFavorite::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
@@ -29,6 +29,8 @@ abstract class AppDatabase : RoomDatabase() {
         }
     }
 
-    abstract fun movieDao(): MovieDao
+    abstract fun matchFavoriteDao(): MatchFavoriteDao
+
+    abstract fun teamFavoriteDao(): MatchFavoriteDao
 
 }
